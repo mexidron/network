9e0d2cfa249e49d81bbed4c7565ab3072c0cfc46
Andrés Pérez <andres.perez.ea1iff@gmail.com>
2016-02-27 16:21:16 +0100

    Added while for waiting to the network to be available

--- src/wifi.py
+++ src/wifi.py
@@ -1,11 +1,14 @@
-WifiName='mexidron'
-WifiPassword='udooers00'
+WifiName='sanjurjo'
+WifiPassword='None'
+NotFound=True
 from wifi import Cell, Scheme
-ap_list=Cell.all('wlan0')
-for item in ap_list:
-    if(item.ssid.find(WifiName) == 0):
-        scheme.delete()
-        scheme = Scheme.for_cell('wlan0',WifiName,item,passkey=WifiPassword)
-        scheme.save()
-
-scheme.activate()
+while (NotFound):
+    ap_list=Cell.all('wlan0')
+    for item in ap_list:
+        if(item.ssid.find(WifiName) == 0):
+            print item
+            NotFound=False
+            scheme.delete()
+            scheme = Scheme.for_cell('wlan0',WifiName,item,passkey=WifiPassword)
+            scheme.save()
+    scheme.activate()
