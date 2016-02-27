796e3687d00b761b57dde77176e9d84cd7ea18aa
Andrés Pérez <andres.perez.ea1iff@gmail.com>
2016-02-27 17:24:20 +0100

    Wifi client powering on and off wifi eth

--- src/wifi.py
+++ src/wifi.py
@@ -1,14 +1,22 @@
 WifiName='sanjurjo'
 WifiPassword='None'
+WirelessETH='wlan0'
 NotFound=True
 from wifi import Cell, Scheme
+import subprocess
+subprocess.call('ifconfig ' + WirelessETH +  ' up'], shell=True)
+os.system(ifconfig )
 while (NotFound):
-    ap_list=Cell.all('wlan0')
+    ap_list=Cell.all(WirelessETH)
     for item in ap_list:
         if(item.ssid.find(WifiName) == 0):
             print item
             NotFound=False
             scheme.delete()
-            scheme = Scheme.for_cell('wlan0',WifiName,item,passkey=WifiPassword)
+            scheme = Scheme.for_cell(WirelessETH,WifiName,item,passkey=WifiPassword)
             scheme.save()
     scheme.activate()
+
+#We call the Upload service
+
+subprocess.call('ifconfig ' + WirelessETH +  ' down', shell=True)
