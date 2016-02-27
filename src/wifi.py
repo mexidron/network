cb86a85d7801d72e1bb7aafdc07a40d915e41af0
Andrés Pérez <andres.perez.ea1iff@gmail.com>
2016-02-27 16:10:32 +0100

    Working WiFi connection

--- src/wifi.py
+++ src/wifi.py
@@ -1,9 +1,11 @@
 WifiName='mexidron'
+WifiPassword='udooers00'
 from wifi import Cell, Scheme
 ap_list=Cell.all('wlan0')
 for item in ap_list:
     if(item.ssid.find(WifiName) == 0):
-        scheme = Scheme.for_cell('wlan0',WifiName, ap_list)
+        scheme.delete()
+        scheme = Scheme.for_cell('wlan0',WifiName,item,passkey=WifiPassword)
         scheme.save()
-        scheme.activate()
 
+scheme.activate()
